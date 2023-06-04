0412995d2fafef7eb540ee2b032e6bfd159b4c57
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-04 23:53:04 +0300

    Several units tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { is_expected.to belong_to(:user).optional(true)}
+
+    it { is_expected.to have_one(:order_detail).dependent(:destroy) }
+
+    it { is_expected.to have_one(:address).through(:order_detail).dependent(:destroy) }
+
+    it { is_expected.to have_many(:product_orders).dependent(:destroy) }
+
+    it { is_expected.to have_many(:products).through(:product_orders).dependent(:destroy) }
+
+    it { is_expected.to accept_nested_attributes_for(:order_detail) }
+  end
+
 end
